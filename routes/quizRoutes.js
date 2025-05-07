ba89bd74bf6e947b1c5c989fdb0c4052e4f9a1f7
DanielLee1712 <lethanhdat170129@gmail.com>
2025-05-07 21:50:09 +0700

    Delete routes directory

--- routes/quizRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getQuizzes, getQuizQuestions, submitQuiz } = require('../controllers/quizController');
-
-router.get('/', getQuizzes);
-router.get('/:quizId/questions', getQuizQuestions);
-router.post('/submit', submitQuiz);
-module.exports = router;
